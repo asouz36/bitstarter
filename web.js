7830b6b727b4d60c9a3cb5ad9f6ccd8becc275d6
Andre Souza <a.souz36@gmail.com>
2013-07-09 18:45:07 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var buffer = new Buffer(26);
+var fs = require('fs');
+
+fs.readFile('./index.html', 'utf8', function (err,data) {
+      if (err) {
+	      return console.log(err);
+	    }
+      buffer.write (data);
+    });
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString('utf-8',0,26));
 });
 
 var port = process.env.PORT || 5000;
